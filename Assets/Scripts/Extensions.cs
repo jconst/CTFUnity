fce4d18bc20a94b3cae5c8a22d54a22ce5558908
Joseph Constantakis <jcon5294@gmail.com>
2014-10-27 18:13:55 -0400

    turret tracks closest enemy

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Extensions
+{
+    // private static System.Random random;
+    // public static float Rand {
+    //     get { 
+    //         if (random == null)
+    //             random = new System.Random(new System.DateTime().Millisecond);
+    //         return (float)(random.NextDouble());
+    //     }
+    // }
+
+    public static float ToAngle(this Vector2 v) {
+        float angle = Vector2.Angle(Vector2.up, v);
+        if (v.x < 0) {
+            angle = 360 - angle;
+        }
+        return angle;
+    }
+    public static Quaternion ToQuaternion(this Vector2 v) {
+        Vector3 euler = new Vector3(0, 0, -v.ToAngle());
+        return Quaternion.Euler(euler);
+    }
+
+    public static Vector2 Abs(this Vector2 v) {
+        return new Vector2(Math.Abs(v.x), Math.Abs(v.y));
+    }
+
+    public static void Each<T>(this IEnumerable<T> ie, Action<T, int> action) {
+        var i = 0;
+        foreach ( var e in ie ) action( e, i++ );
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Turret : MonoBehaviour
+{
+    public string color;
+    public float fireFreq;
+
+    void Start() {
+        
+    }
+    
+    void Update() {
+        Vector2 toTarget = ClosestEnemyPlayer().transform.position - transform.position;
+        transform.rotation = toTarget.ToQuaternion();
+    }
+
+    Player ClosestEnemyPlayer() {
+        List<Player> players = (GameObject.FindObjectsOfType(typeof(Player)) as Player[]).ToList();     
+        return players.Where(p => p.color != color)
+                      .Aggregate((closestSoFar, cur) => HowFar(cur) < HowFar(closestSoFar) ? cur : closestSoFar);
+    }
+
+    float HowFar(Player p) {
+        return (transform.position - p.transform.position).magnitude;
+    }
+}

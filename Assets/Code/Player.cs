5395588221db5dd6c8315b10384f7a3c89d64c82
unknown <deboltj@umich.edu>
2014-10-27 12:54:58 -0400

    Scoooooooore

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public string controllerNum;
+	public string color;
+	public string otherColor;
+	public Vector3 initialPos;
+	public GUIText score;
+	
+	public GameObject item1;
+	public GameObject item2;
+	public GameObject item3;
+	public GameObject item4;
+
+	float speed=4f;
+	bool carrying=false;
+	Flag flag;
+	bool safe=true;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+		score = GameObject.Find (color + "Score").GetComponent<GUIText> ();
+
+		if (!item1) {
+					item1 = new GameObject ();
+				}
+		if (!item2) {
+			item2 = new GameObject ();
+		}
+		if (!item3) {
+			item3 = new GameObject ();
+		}
+		if (!item4) {
+			item4 = new GameObject ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocity = Vector3.zero;
+		Vector3 pos = transform.position;
+		velocity.x = Input.GetAxis ("Horizontal" + controllerNum)*speed;
+		velocity.y = Input.GetAxis ("Vertical" + controllerNum)*speed;
+		pos += velocity * Time.deltaTime;
+		transform.position = pos;
+
+		if(Input.GetButtonDown("Item1"+controllerNum))
+		   {
+			GameObject go= Instantiate(item1) as GameObject;
+			go.transform.position=transform.position;
+		}
+		if(Input.GetButtonDown("Item2"+controllerNum))
+		{
+			GameObject go= Instantiate(item2) as GameObject;
+			go.transform.position=transform.position;
+		}
+		if(Input.GetButtonDown("Item3"+controllerNum))
+		{
+			GameObject go= Instantiate(item3) as GameObject;
+			go.transform.position=transform.position;
+		}
+		if(Input.GetButtonDown("Item4"+controllerNum))
+		{
+			GameObject go= Instantiate(item4) as GameObject;
+			go.transform.position=transform.position;
+		}
+
+		Vector3 relativePos = Camera.main.WorldToViewportPoint (transform.position);
+
+		if (relativePos.x > 1f)
+						relativePos.x = 1f;
+		if (relativePos.y > 1f)
+			relativePos.y = 1f;
+		if (relativePos.x < 0f)
+			relativePos.x = 0f;
+		if (relativePos.y < 0f)
+			relativePos.y = 0f;
+		transform.position = Camera.main.ViewportToWorldPoint (relativePos);
+
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("dongs");
+		if (col.CompareTag (color + "Side")) {
+						safe = true;
+			Debug.Log ("dongsyay");
+				}
+		
+		if (col.CompareTag (otherColor + "Side")) {
+						safe = false;
+			Debug.Log ("dongspoop");
+				}
+		if (col.CompareTag ("Player")) 
+		{
+			if(!safe&&col.gameObject.GetComponent<Player>().color.Equals(otherColor))
+				KillPlayer();
+		}
+		if (col.CompareTag (otherColor + "Flag")) {
+			carrying=true;
+			col.gameObject.GetComponent<Flag>().Pickup(this.gameObject);
+			flag=col.gameObject.GetComponent<Flag>();
+				}
+		if (col.CompareTag (color + "Flag") && carrying) {
+			carrying=false;
+			flag.Reset();
+			int sc=  int.Parse (score.text);
+			sc+=1;
+			score.text=sc.ToString();
+			flag=null;
+				}
+	}
+
+	public void KillPlayer()
+	{
+		GameObject player = Instantiate (this.gameObject) as GameObject;
+		player.transform.position = initialPos;
+		Player p = player.GetComponent<Player> ();
+		p.initialPos = initialPos;
+		p.carrying = false;
+		p.speed = 4f;
+		if (carrying)
+						flag.Reset ();
+		Destroy (this.gameObject);
+	}
+}

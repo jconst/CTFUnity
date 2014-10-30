4ece2ffa98e369cc1e0d092516b84d61d9e63562
Joseph Constantakis <jcon5294@gmail.com>
2014-10-29 21:20:56 -0400

    add keyboard control of player 1 for debugging

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,8 +25,8 @@ public class Player : MonoBehaviour {
 		initialPos = transform.position;
 		score = GameObject.Find (color + "Score").GetComponent<GUIText> ();
 		if (!item1) {
-					item1 = new GameObject ();
-				}
+			item1 = new GameObject ();
+		}
 		if (!item2) {
 			item2 = new GameObject ();
 		}
@@ -44,11 +44,18 @@ public class Player : MonoBehaviour {
 		Vector3 pos = transform.position;
 		velocity.x = Input.GetAxis ("Horizontal" + controllerNum)*speed;
 		velocity.y = Input.GetAxis ("Vertical" + controllerNum)*speed;
+
+		//allow p1 to be controlled by keyboard for testing:
+		if (controllerNum == "1" && velocity.magnitude == 0) {
+			velocity.x = Input.GetAxisRaw("Horizontal")*speed;
+			velocity.y = Input.GetAxisRaw("Vertical")*speed;
+		}
+
 		pos += velocity * Time.deltaTime;
 		transform.position = pos;
 
 		if(Input.GetButtonDown("Item1"+controllerNum))
-		   {
+		{
 			GameObject go= Instantiate(item1) as GameObject;
 			go.transform.position=transform.position;
 		}
@@ -71,7 +78,7 @@ public class Player : MonoBehaviour {
 		Vector3 relativePos = Camera.main.WorldToViewportPoint (transform.position);
 
 		if (relativePos.x > 1f)
-						relativePos.x = 1f;
+			relativePos.x = 1f;
 		if (relativePos.y > 1f)
 			relativePos.y = 1f;
 		if (relativePos.x < 0f)
@@ -79,22 +86,20 @@ public class Player : MonoBehaviour {
 		if (relativePos.y < 0f)
 			relativePos.y = 0f;
 		transform.position = Camera.main.ViewportToWorldPoint (relativePos);
-
-	
 	}
 
 	void OnTriggerEnter(Collider col)
 	{
 		Debug.Log ("dongs");
 		if (col.CompareTag (color + "Side")) {
-						safe = true;
+			safe = true;
 			Debug.Log ("dongsyay");
-				}
+		}
 		
 		if (col.CompareTag (otherColor + "Side")) {
-						safe = false;
+			safe = false;
 			Debug.Log ("dongspoop");
-				}
+		}
 		if (col.CompareTag ("Player")) 
 		{
 			if(!safe&&col.gameObject.GetComponent<Player>().color.Equals(otherColor))
@@ -104,7 +109,7 @@ public class Player : MonoBehaviour {
 			carrying=true;
 			col.gameObject.GetComponent<Flag>().Pickup(this.gameObject);
 			flag=col.gameObject.GetComponent<Flag>();
-				}
+		}
 		if (col.CompareTag (color + "Flag") && carrying) {
 			carrying=false;
 			int sc = int.Parse(score.text);
@@ -112,7 +117,7 @@ public class Player : MonoBehaviour {
 			score.text=sc.ToString();
 			flag.Reset();
 			flag=null;
-				}
+		}
 	}
 
 	public void KillPlayer()

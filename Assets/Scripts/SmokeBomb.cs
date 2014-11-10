3c2b47efc314e847d08a87189da888cc9d5c908b
Joseph Constantakis <jcon5294@gmail.com>
2014-11-10 11:44:01 -0500

    fix smokebomb fadeout

--- Assets/Scripts/SmokeBomb.cs
+++ Assets/Scripts/SmokeBomb.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class SmokeBomb : DropItem {
-	private float speed = 0.6f;
+	private float speed = 1f;
+    private float fadeoutStartTime = 2f;
 
 	void Update() {
 		UpdateTime();
@@ -15,23 +16,15 @@ public class SmokeBomb : DropItem {
 
     void BlowUp() {
     	if (lifeTime <= 0) {
-			Destroy (this.gameObject);
-		} else {
-			transform.localScale += Vector3.one * 0.8f * speed * Time.deltaTime;
+			Destroy(gameObject);
+		}
+		transform.localScale += Vector3.one * speed * Time.deltaTime;
 
-			if (lifeTime <= 4) {
-				Color color = gameObject.renderer.material.color;
-				color.a -= 0.01f;
-				gameObject.renderer.material.color = color;
-			}
+		if (lifeTime <= fadeoutStartTime) {
+			Color color = gameObject.renderer.material.color;
+			color.a -= (1f/fadeoutStartTime) * Time.deltaTime;
+            Debug.Log(color.a);
+			gameObject.renderer.material.color = color;
 		}
     }
-
-    /*void OnTriggerEnter2D (Collider2D coll) {
-		Player player = coll.GetComponent<Player>();
-
-		if (player && time >= safetime) {
-			player.KillPlayer();
-		}
-	}*/
 }

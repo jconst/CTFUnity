66a9b9067fea4a5aeca16f2e78f5afffbf0a8480
Joseph Constantakis <jcon5294@gmail.com>
2014-12-08 19:32:05 -0500

    add glow (for better or worse), create prefabs for walls

---
+++ Assets/Editor/SnapToGrid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+ 
+public class SnapToGrid : ScriptableObject {
+ 
+    const float snapGranularity = 0.25f;
+
+    [MenuItem ("Window/Snap to Grid %g")]
+    static void MenuSnapToGrid() {
+        foreach (Transform t in Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable)) {
+            t.position = new Vector3 (
+                Mathf.Round(t.position.x / snapGranularity) * snapGranularity,
+                Mathf.Round(t.position.y / snapGranularity) * snapGranularity,
+                Mathf.Round(t.position.z / snapGranularity) * snapGranularity
+            );
+        }
+    }
+}
